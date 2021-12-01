e05b609b29501f7c7c3b812abbf2aba9fba7fc28
Pendam Nithin Kumar <nithin.pendam@gmail.com>
2021-12-01 10:18:39 +0530

    Detailed

--- sprint2/files/format.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION format_phone ( p_phone_no IN OUT VARCHAR2 ) RETURN VARCHAR2
-PARALLEL_ENABLE -- Optimizer HINT 
-IS
-BEGIN
-	p_phone_no := '+91('||SUBSTR(p_phone_no,1,3)||')'||SUBSTR(p_phone_no,4,3)||'-'||SUBSTR(p_phone_no,7);
-	RETURN p_phone_no;
-END format_phone;
-/
--- sprint2/files/new.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE new (p_deptno NUMBER, p_table VARCHAR2) IS
-v_count NUMBER(2);
-v_sql VARCHAR2(100);
-BEGIN
-v_sql := 'SELECT COUNT(*) FROM :p_table WHERE deptno = :DEPT';
-EXECUTE IMMEDIATE v_sql INTO v_count USING p_table,p_deptno;
-DBMS_OUTPUT.PUT_LINE('COUNT :'||v_count);
-END;
-/
